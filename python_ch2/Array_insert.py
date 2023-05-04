3fa899c778898cc3012cc7e0b83b91172fa6289b
maysaa1992 <batainehmaysaa@gmail.com>
2023-05-04 13:14:23 +0000

    ch2

---
+++ python_ch2/Array_insert.py
@@ -0,0 +1,18 @@
+def insertion (arr ,num):
+ print (arr)   
+ for x in range(len (arr)):
+  if arr[x]>num and arr[x-1]<num :
+    #arr[x-1] = num
+    arr.insert(x,num)
+    break
+  else:
+    continue
+    
+  
+ print(arr)    
+    
+    
+    
+insertion([1,2,4],3)  
+insertion([42,8,15,23,42], 16)  
+insertion([2,4,6,-8], 5)
