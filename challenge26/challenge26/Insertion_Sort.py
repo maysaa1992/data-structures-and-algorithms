81de5f91d77cc5a0376fd51ac6f671ecfd88ffbd
maysaa1992 <batainehmaysaa@gmail.com>
2023-07-10 19:41:33 +0000

    ch26

---
+++ challenge26/challenge26/Insertion_Sort.py
@@ -0,0 +1,19 @@
+def insertsort(arr, value):
+    arr.append(value)
+    
+    length = len(arr)
+    
+    # Iterate over each element starting from the second one
+    for i in range(1, length):
+        key = arr[i]  
+        
+        
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]  
+            j = j - 1
+        
+        arr[j + 1] = key  
+    return arr
+
+print(insertsort([20, 18, 12, 8, 5, -2], 3))
---
+++ challenge26/test/test.py
@@ -0,0 +1,6 @@
+import pytest
+from challenge26.Insertion_Sort import insertsort
+def test_Reverse_sorted():
+    actual=insertsort([20,18,12,8,5,-2],3)
+    expected= [-2, 3, 5, 8, 12, 18, 20]
+    assert actual== expected
